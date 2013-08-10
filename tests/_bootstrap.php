a9b82b9085d59ea4ebfb38cc4697eca9de5d3cc8
Kogo Toki <kogotoki@ya.ru>
2013-08-10 09:48:08 +0400

    Debug travis

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -2,7 +2,7 @@
 use Bararai\Tests\Acceptance\Bootstrap\SettingsHelper;
 use Bararai\Tests\Acceptance\Object\Admin;
 
-$settingsHelper = new SettingsHelper(__DIR__ . '/../');
+$settingsHelper = new SettingsHelper(__DIR__ . '/..');
 $settingsHelper->setPassword(Admin::$password);
 $settingsHelper->setDbFromConfig();
 $settingsHelper->setInstalled();
