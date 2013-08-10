f631636a76b7f9871141e339d18b82c1c4857a66
Kogo Toki <kogotoki@ya.ru>
2013-08-10 09:59:24 +0400

    Debug travis

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -2,9 +2,15 @@
 use Bararai\Tests\Acceptance\Bootstrap\SettingsHelper;
 use Bararai\Tests\Acceptance\Object\Admin;
 
-$settingsHelper = new SettingsHelper(__DIR__ . '/..');
+$projectRoot = realpath(__DIR__ . '/..');
+echo $projectRoot, "\n";
+$settingsHelper = new SettingsHelper($projectRoot);
 $settingsHelper->setPassword(Admin::$password);
 $settingsHelper->setDbFromConfig();
 $settingsHelper->setInstalled();
+
+echo file_get_contents($projectRoot.'/vendor/e2/e2/user/instance.psa');
+echo file_get_contents($projectRoot.'/vendor/e2/e2/user/settings.psa');
+echo file_get_contents($projectRoot.'/vendor/e2/e2/user/password-hash.psa');
 
 unset($settingsHelper);
