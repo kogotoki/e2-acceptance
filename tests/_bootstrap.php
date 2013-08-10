721b77580829589780b93d2ee82d3c7cd7894628
Kogo Toki <kogotoki@ya.ru>
2013-08-10 11:03:52 +0400

    Found reason of travis fail — e2 require 'short_open_tag=On'

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -2,16 +2,9 @@
 use Bararai\Tests\Acceptance\Bootstrap\SettingsHelper;
 use Bararai\Tests\Acceptance\Object\Admin;
 
-$projectRoot = realpath(__DIR__ . '/..');
-echo $projectRoot, "\n";
-$settingsHelper = new SettingsHelper($projectRoot);
+$settingsHelper = new SettingsHelper(realpath(__DIR__ . '/..'));
 $settingsHelper->setPassword(Admin::$password);
 $settingsHelper->setDbFromConfig();
 $settingsHelper->setInstalled();
-
-echo file_get_contents($projectRoot.'/vendor/e2/e2/user/instance.psa');
-echo file_get_contents($projectRoot.'/vendor/e2/e2/user/settings.psa');
-echo file_get_contents($projectRoot.'/vendor/e2/e2/user/password-hash.psa');
-echo file_get_contents('http://localhost:4545/');
 
 unset($settingsHelper);
